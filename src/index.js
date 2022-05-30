a63df357c7c2f5f316e49fec16860cc3747c65e1
Sidh <danduhman7@gmail.com>
2022-05-30 16:50:27 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,85 @@
+export function smug() {
+  var img = document.createElement("img");
+  img.src ="https://cdn.discordapp.com/emojis/760355822500577280.webp?size=96&quality=lossless";
+  document.body.appendChild(img).width = "40";
+}
+export function clown() {
+  var img = document.createElement("img");
+  img.src = "https://cdn3.emoji.gg/emojis/2901-clown-peepo.png";
+  document.body.appendChild(img).width = "40";
+}
+export function hmm() {
+  var img = document.createElement("img");
+  img.src = "https://cdn3.emoji.gg/emojis/9539-peepoukraine.png";
+  document.body.appendChild(img).width = "40";
+}
+export function Ukraine() {
+  var img = document.createElement("img");
+  img.src = "https://cdn3.emoji.gg/emojis/3959-hmm.png";
+  document.body.appendChild(img).width = "40";
+}
+export function monkaThink() {
+  var img = document.createElement("img");
+  img.src = "https://cdn3.emoji.gg/emojis/7739-monkathink.png";
+  document.body.appendChild(img).width = "40";
+}
+export function pepe() {
+  var img = document.createElement("img");
+  img.src = "https://cdn3.emoji.gg/emojis/9529-pepe.png";
+  document.body.appendChild(img).width = "40";
+}
+export function tearsOfJoy() {
+  var img = document.createElement("img");
+  img.src = "https://cdn3.emoji.gg/emojis/7864-pepetearsofjoy.png";
+  document.body.appendChild(img).width = "40";
+}
+export function look() {
+  var img = document.createElement("img");
+  img.src = "https://cdn3.emoji.gg/emojis/2247-pepelook.png";
+  document.body.appendChild(img).width = "40";
+}
+export function call() {
+  var img = document.createElement("img");
+  img.src = "https://cdn3.emoji.gg/emojis/8410-pepe-phone202.png";
+  document.body.appendChild(img).width = "40";
+}
+export function shoot() {
+  var img = document.createElement("img");
+  img.src = "https://cdn3.emoji.gg/emojis/9317-pepegaaim.png";
+  document.body.appendChild(img).width = "40";
+}
+export function ree() {
+  var img = document.createElement("img");
+  img.src = "https://cdn3.emoji.gg/emojis/7509-pepetriggered.png";
+  document.body.appendChild(img).width = "40";
+}
+export function astro() {
+  var img = document.createElement("img");
+  img.src = "https://cdn3.emoji.gg/emojis/8663-astropepe.png";
+  document.body.appendChild(img).width = "40";
+}
+export function enlightment() {
+  var img = document.createElement("img");
+  img.src = "https://cdn3.emoji.gg/emojis/5482-pepeenlightenment.png";
+  document.body.appendChild(img).width = "40";
+}
+export function noted() {
+  var img = document.createElement("img");
+  img.src = "https://cdn3.emoji.gg/emojis/7455-pepenoted.png";
+  document.body.appendChild(img).width = "40";
+}
+export function sus() {
+  var img = document.createElement("img");
+  img.src = "https://cdn3.emoji.gg/emojis/1569-peeposuslook.png";
+  document.body.appendChild(img).width = "40";
+}
+export function dumb() {
+  var img = document.createElement("img");
+  img.src = "https://cdn3.emoji.gg/emojis/3916-pepedumb.png";
+  document.body.appendChild(img).width = "40";
+}
+export function cool() {
+  var img = document.createElement("img");
+  img.src = "https://cdn3.emoji.gg/emojis/4094-pepe-14.png";
+  document.body.appendChild(img).width = "40";
+}
