051ec2f0240ef5eb6d612566f8c452c7c73dbb85
Sidh <danduhman7@gmail.com>
2022-05-30 16:50:57 +0530

    Add files via upload

---
+++ test/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <smug  />
+    <clown />
+    <hmm />
+    <Ukraine />
+    <monkaThink />
+    <pepe />
+    <tearsOfJoy />
+    <look />
+    <call />
+    <shoot />
+    <ree />
+    <astro />
+    <enlightment />
+    <noted />
+    <sus />
+    <dumb />
+    <cool />
+  </div>
+</template>
+
+<script>
+import { smug, clown, hmm, Ukraine, monkaThink, pepe, tearsOfJoy, look, call, shoot, ree, astro, enlightment, noted, sus, dumb, cool } from "../../src/index";
+
+export default {
+  name: "App",
+  components: {
+    smug,
+    clown,
+    hmm,
+    Ukraine,
+    monkaThink,
+    pepe,
+    tearsOfJoy,
+    look,
+    call,
+    shoot,
+    ree,
+    astro,
+    enlightment,
+    noted,
+    sus,
+    dumb,
+    cool
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  background-color: rgba(0, 0, 0, 0.874);
+}
+</style>
